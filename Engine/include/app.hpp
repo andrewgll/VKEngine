b5206a3db1a5405a44a92e6526311165900c2961
Andrii <andrii.hladkyi.teacher@gmail.com>
2024-12-20 19:17:33 -0500

    swap chain

--- Engine/include/app.hpp
+++ Engine/include/app.hpp
@@ -3,6 +3,11 @@
 #include "window.hpp"
 #include "pipeline.hpp"
 #include "device.hpp"
+#include "swap_chain.hpp"
+
+//std
+#include <memory>  
+#include <vector>
 
 namespace vke{
     class App{
@@ -10,14 +15,23 @@ namespace vke{
             static constexpr int WIDTH = 800;
             static constexpr int HEIGHT = 600;
             
+            App();
+            ~App();
+
+            App(const App &) = delete;
+            App &operator = (const App &) = delete;
             void run(); 
         private:
+            void createPipelineLayout();
+            void createPipeline();
+            void createCommandBuffers();
+            void drawFrame();
+
             VkeWindow vkeWindow{WIDTH, HEIGHT, "YOPTA"};
             VkeDevice vkeDevice{vkeWindow};
-            VkePipeline vkePipeline{
-                vkeDevice, 
-                "Engine/shaders/shader.vert.spv", 
-                "Engine/shaders/shader.frag.spv", 
-                VkePipeline::defaultPipelineConfigInfo(WIDTH, HEIGHT)};
+            VkeSwapChain vkeSwapChain{vkeDevice, vkeWindow.getExtent()};
+            std::unique_ptr<VkePipeline> vkePipeline;
+            VkPipelineLayout pipelineLayout;
+            std::vector<VkCommandBuffer> commandBuffer;
     };
 } // namespace vke
--- Engine/src/app.cpp
+++ Engine/src/app.cpp
@@ -1,9 +1,47 @@
 #include "app.hpp"
 
+//std
+#include <stdexcept>
+
 namespace vke{
+    App::App(){
+        createPipelineLayout();
+        createPipeline();
+        createCommandBuffers();
+    }
+    App::~App(){
+        vkDestroyPipelineLayout(vkeDevice.device(), pipelineLayout, nullptr);
+     }
     void App::run() {
         while(!vkeWindow.shouldClose()){
             glfwPollEvents();
         }
     }
+
+    void App::createPipelineLayout(){
+        VkPipelineLayoutCreateInfo pipelineLayoutInfo{};
+        pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
+        pipelineLayoutInfo.setLayoutCount = 0;
+        pipelineLayoutInfo.pSetLayouts = nullptr;
+        pipelineLayoutInfo.pushConstantRangeCount = 0;
+        pipelineLayoutInfo.pPushConstantRanges = nullptr;
+        if(vkCreatePipelineLayout(vkeDevice.device(), &pipelineLayoutInfo, nullptr, &pipelineLayout) != VK_SUCCESS){
+            throw std::runtime_error("failed to create pipeline layout");
+        }
+    }
+
+    void App::createPipeline(){
+        auto pipelineConfig = VkePipeline::defaultPipelineConfigInfo(vkeSwapChain.width(), vkeSwapChain.height());
+        pipelineConfig.renderPass = vkeSwapChain.getRenderPass(); 
+        pipelineConfig.pipelineLayout = pipelineLayout;
+        vkePipeline = std::make_unique<VkePipeline>(
+            vkeDevice,
+            "Engine/shaders/shader.vert.spv",
+            "Engine/shaders/shader.frag.spv",
+            pipelineConfig
+        );
+    }
+
+    void App::createCommandBuffers() {}
+    void App::drawFrame() {}
 }
